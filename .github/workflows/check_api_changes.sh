f88b50793086bba306bfb9ecd8f22c5bf9c084c8
brmorg-to <68782874+brmorg-to@users.noreply.github.com>
2023-05-29 14:41:53 -0400

    Add files via upload

--- .github/workflows/check_api_changes.sh
+++ .github/workflows/check_api_changes.sh
@@ -3,11 +3,29 @@
 # get the last commit hash
 last_commit=$(git rev-parse HEAD)
 
+# ensure the variable is assigned
+if [ -z "$last_commit" ]; then
+  echo "Failed to get last commit"
+  exit 1
+fi
+
 # get the parent commit hash
-parent_commit=$(git rev-parse "$last_commit"^)
+parent_commit=$(git rev-parse "${last_commit}^")
+
+# ensure the variable is assigned
+if [ -z "$parent_commit" ]; then
+  echo "Failed to get parent commit"
+  exit 1
+fi
 
 # get the diff
 diff=$(git diff "$parent_commit" "$last_commit" --name-only -- '*.java')
+
+# ensure the variable is assigned
+if [ -z "$diff" ]; then
+  echo "Failed to get diff"
+  exit 1
+fi
 
 # loop through each file in the diff
 for file in $diff; do
@@ -17,3 +35,4 @@ for file in $diff; do
         grep "@RequestMapping\|@RestController" "$file" >> api_changes_report.txt
     fi
 done
+
